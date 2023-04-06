d5b22f7f9f321153d78c76583801b5008c279877
Dmitriy <merk.dima15@gmail.com>
2023-04-06 17:27:06 +0300

    Zadacha66

---
+++ Zadacha66.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SumOfNaturalNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение M: ");
+            int m = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите значение N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = CalculateSum(m, n);
+            Console.WriteLine($"Сумма натуральных чисел в промежутке от {m} до {n}: {sum}");
+        }
+
+        static int CalculateSum(int m, int n)
+        {
+            int sum = 0;
+            for (int i = m; i <= n; i++)
+            {
+                if (i > 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+    }
+}
