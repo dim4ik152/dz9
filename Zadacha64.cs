f8da00112996e14d844d1fcae52f206841f02117
Dmitriy <merk.dima15@gmail.com>
2023-04-06 17:24:58 +0300

    Zadacha64

---
+++ Zadacha64.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RecursiveCountdown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            PrintNumbersRecursively(n);
+        }
+
+        static void PrintNumbersRecursively(int n)
+        {
+            if (n > 0)
+            {
+                Console.WriteLine(n);
+                PrintNumbersRecursively(n - 1);
+            }
+        }
+    }
+}
